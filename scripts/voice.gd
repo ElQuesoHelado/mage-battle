70b8ba9c2c6e74bb1cf3e616f7bbc4662bdab414
Jose Alva Cornejo <33399866+ElQuesoHelado@users.noreply.github.com>
2026-09-14 21:34:35 -0500

    Grammar en vosk

--- scripts/voice.gd
+++ scripts/voice.gd
@@ -1,21 +1,59 @@
 extends Node3D
 
-@onready var speech_recognizer := $SpeechRecognizer as SpeechRecognizer
+#@onready var speech_recognizer := $SpeechRecognizer as SpeechRecognizer
 @onready var player := $AudioStreamPlayer as AudioStreamPlayer
 @onready var camera := $XROrigin3D/XRCamera3D as XRCamera3D
 
 const MONO_MESH := preload("res://assets/Orangutan.obj")
 
+var _effect: AudioEffectCapture
+var _recognizer := VoskRecognizer.new()
+
 func _ready() -> void:
 	player.play()
 
-func _on_speech_recognizer_result(confidence: float, text: String) -> void:
-	print("result: ", text)
-	#if text.to_lower().contains("mono"):
-	spawn_mono()
+	var model := load("res://assets/vosk-model-small-es-0.42.vosk") as VoskModel
+	var mix_rate: float = ProjectSettings.get_setting("audio/driver/mix_rate", 44100)
 
-func _on_speech_recognizer_partial_result(text: String) -> void:
-	print("partial: ", text)
+	var grammar := PackedStringArray(["mono","fuego", "esperma", "[unk]"])
+	var error := _recognizer.setup_with_grammar(model, mix_rate, grammar)
+	if error != OK:
+		push_error("failed to set up Vosk recognizer with grammar")
+		return
+
+	var bus_idx := AudioServer.get_bus_index("Record")
+	for i in AudioServer.get_bus_effect_count(bus_idx):
+		var fx := AudioServer.get_bus_effect(bus_idx, i)
+		if fx is AudioEffectCapture:
+			_effect = fx
+			break
+
+	if _effect == null:
+		push_error("no AudioEffectCapture found on bus 'Record'")
+
+func _process(_delta: float) -> void:
+	if _effect == null:
+		return
+
+	var samples := _effect.get_buffer(_effect.get_frames_available())
+	if _recognizer.accept_samples(samples) == OK:
+		var alternatives = _recognizer.get_final_result().get("alternatives", [])
+		if alternatives.size() > 0:
+			var text: String = alternatives[0].get("text", "")
+			if text != "" and text.to_lower().contains("esperma"):
+				spawn_mono()
+
+
+#func _ready() -> void:
+	#player.play()
+
+#func _on_speech_recognizer_result(confidence: float, text: String) -> void:
+	#print("result: ", text)
+	##if text.to_lower().contains("mono"):
+	#spawn_mono()
+#
+#func _on_speech_recognizer_partial_result(text: String) -> void:
+	#print("partial: ", text)
 
 func spawn_mono() -> void:
 	var from := camera.global_transform.origin
